7a9737d3f068c1f6bf0a577e2c809ad05b530d62
vismayatk002 <vismayatk002@gmail.com>
2021-08-06 01:23:01 +0530

    Refactor : edit contact

--- AddressBook.java
+++ AddressBook.java
@@ -1,3 +1,4 @@
+import java.util.*;
 public class AddressBook extends Contact{
 
     String address, city, state;
@@ -35,5 +36,35 @@ public class AddressBook extends Contact{
         return zip;
     }
     
+    public void showContact(AddressBook contact){
 
+        System.out.print("\n-----------------");
+        System.out.print("\nFirst Name  : " +  contact.getFirstName());
+        System.out.print("\nLast Name   : " +  contact.getLastName());
+        System.out.print("\nAddress     : " +  contact.getAddress());
+        System.out.print("\nCity        : " +  contact.getCity());
+        System.out.print("\nState       : " +  contact.getState());
+        System.out.print("\nPone Number : " +  contact.getPhoneNo());
+        System.out.print("\nE-mail      : " +  contact.getEmail());
+        System.out.print("\nZip         : " +  contact.getZip());
+    }
+    public void editContact(AddressBook[] contactArr){
+
+        Scanner sc= new Scanner(System.in);
+        System.out.print("\nEnter the First name for update Address : ");
+        String editName = sc.nextLine();
+        int flag = 1;
+        for(int i=0; i<contactArr.length; i++){
+            if(editName.equals(contactArr[i].getFirstName())){
+                System.out.print("\nEnter your new Address : ");
+                address = sc.nextLine();
+                contactArr[i].setAddress(address);
+                flag = 0;
+            }
+        }
+        if(flag == 1){
+            System.out.print("\ncouldn't find the contact..");
+        }
+        sc.close();
+    }
 }
--- AddressBookMain.java
+++ AddressBookMain.java
@@ -6,52 +6,61 @@ public class AddressBookMain{
         String firstName, lastName, address, city, state, email;
         int zip;
         long phoneNo;
+        //array to store object of type AddressBook
+        AddressBook[] contactArr = new AddressBook[2];
 
         Scanner sc= new Scanner(System.in);
-        AddressBook contact = new AddressBook();
-        System.out.print("Enter First Name  : ");  
-        firstName = sc.nextLine();
-        contact.setFirstName(firstName);
+        for(int i=0; i<contactArr.length; i++){
+           
+            //create new object in each iteration
+            AddressBook contact = new AddressBook();
 
-        System.out.print("\nEnter Last Name  : ");  
-        lastName = sc.nextLine();
-        contact.setFirstName(lastName);
+            System.out.print("\nEnter First Name  : ");  
+            firstName = sc.nextLine();
+            contact.setFirstName(firstName);
 
-        System.out.print("\nEnter E-mail  : ");
-        email = sc.nextLine();
-        contact.setEmail(email);
+            System.out.print("\nEnter Last Name  : ");  
+            lastName = sc.nextLine();
+            contact.setLastName(lastName);
 
-        System.out.print("\nEnter Address  : ");  
-        address = sc.nextLine(); 
-        contact.setAddress(address);
+            System.out.print("\nEnter E-mail  : ");
+            email = sc.nextLine();
+            contact.setEmail(email);
 
-        System.out.print("\nEnter City  : ");  
-        city = sc.nextLine(); 
-        contact.setCity(city);
+            System.out.print("\nEnter Address  : ");  
+            address = sc.nextLine(); 
+            contact.setAddress(address);
 
-        
+            System.out.print("\nEnter City  : ");  
+            city = sc.nextLine(); 
+            contact.setCity(city);
 
-        System.out.print("\nEnter State  : ");  
-        state = sc.nextLine();
-        contact.setState(state);
+            System.out.print("\nEnter State  : ");  
+            state = sc.nextLine();
+            contact.setState(state);
 
-        System.out.print("\nEnter Phone Number  : ");   
-        phoneNo = sc.nextLong();
-        contact.setPhoneNo(phoneNo);
+            System.out.print("\nEnter Phone Number  : ");   
+            phoneNo = sc.nextLong();
+            contact.setPhoneNo(phoneNo);
 
-        System.out.print("\nEnter Zip  : ");  
-        zip = sc.nextInt(); 
-        contact.setZip(zip);
+            System.out.print("\nEnter Zip  : ");  
+            zip = sc.nextInt(); 
+            contact.setZip(zip);
 
-        sc.close();
+            sc.nextLine();
+            
+            contactArr[i] = contact;
+        }
+        System.out.print("\nDo you want to edit? press Y / N : ");
+        char editOption = sc.next().charAt(0);
+        AddressBook person = new AddressBook();
+        if(editOption == 'Y'){
+            person.editContact(contactArr);
+        }
+        for(int i=0; i<contactArr.length; i++){
 
-        System.out.print("\nFirst Name  : " +  contact.getFirstName());
-        System.out.print("\nLast Name   : " +  contact.getLastName());
-        System.out.print("\nAddress     : " +  contact.getAddress());
-        System.out.print("\nCity        : " +  contact.getCity());
-        System.out.print("\nState       : " +  contact.getState());
-        System.out.print("\nPone Number : " +  contact.getPhoneNo());
-        System.out.print("\nE-mail      : " +  contact.getEmail());
-        System.out.print("\nZip         : " +  contact.getZip());
+           contactArr[i].showContact(contactArr[i]);
+        }
+    sc.close();
     }
 }
